0ca34994f8cc9483389dafb374a16afc36bcd324
emmy <52672675+emmettgb@users.noreply.github.com>
2022-07-07 15:02:46 -0500

    updated compat and docs

--- src/ToolipsUploader.jl
+++ src/ToolipsUploader.jl
@@ -1,8 +1,37 @@
+"""
+Created in July, 2022 by
+[chifi - an open source software dynasty.](https://github.com/orgs/ChifiSource)
+by team
+[toolips](https://github.com/orgs/ChifiSource/teams/toolips)
+This software is MIT-licensed.
+### ToolipsUploader
+The toolips uploader provides both a server extension for handling incoming server
+    uploads, as well as some component upload buttons that can be written to send
+    files to the server.
+##### Module Composition
+- [**Toolips**](https://github.com/ChifiSource/Toolips.jl)
+"""
 module ToolipsUploader
 using Toolips
 import Toolips: ServerExtension
 using ToolipsSession
 
+"""
+### Uploader <: Toolips.ServerExtension
+- type::Vector{Symbol} - the type of this extension.
+- directory::String - The directory to put temporary uploads.
+- lastupload::Dict{String, String} - A dictionary containing ips and files for the last
+upload of each `Connection`.
+- f::Function - The routing extension function that creates the upload route.\n
+The Uploader ServerExtension handles incoming client uploads on the server side.
+##### example
+```
+
+```
+------------------
+##### constructors
+- Uploader(directory::String = "uploads", upload_f::Function = uploadsave)
+"""
 mutable struct Uploader <: ServerExtension
     type::Vector{Symbol}
     directory::String
@@ -18,6 +47,33 @@ mutable struct Uploader <: ServerExtension
     end
 end
 
+"""
+**Uploader Interface**
+### fileinput(f::Function, c::Connection, name::String = "")
+------------------
+Creates a new fileinput Component, which will upload files to an `Uploader`.
+The `f` function should take a ComponentModifier and a String as positional
+arguments. Files will be removed automatically so long as an exception is not
+thrown.
+#### example
+```
+myuploader = ToolipsUploader.fileinput(c,
+"pizza") do cm::ComponentModifier, file::String
+    try
+        readstr = read(file, String)
+        style!(cm, uploaderbox, "height" => "250px")
+        set_children!(cm, "uploaderbox", components(tmd("customtmd", readstr)))
+    catch
+        rm(file)
+        errora = a("errora",
+        text = "Error! You probably uploaded the wrong type of file, didn't you?")
+        style!(errora, "color" => "red")
+        style!(cm, uploaderbox, "height" => "20px")
+        set_children!(cm, "uploaderbox", components(errora))
+    end
+end
+```
+"""
 function fileinput(f::Function, c::Connection, name::String = "")
     inp::Component = input(name * "input", type = "file", name = "fname")
     inp["oninput"] = """readFile(this);"""
