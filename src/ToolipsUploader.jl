e208bb168dbfd568b18fb0801d71ae631b2035eb
emmy <52672675+emmettgb@users.noreply.github.com>
2022-11-21 21:34:34 -0600

    Add files via upload

--- src/ToolipsUploader.jl
+++ src/ToolipsUploader.jl
@@ -12,34 +12,49 @@ The toolips uploader provides both a server extension for handling incoming serv
 - [**Toolips**](https://github.com/ChifiSource/Toolips.jl)
 """
 module ToolipsUploader
-using Toolips
+using ToolipsDefaults
 import Base: read
 import Toolips: ServerExtension, AbstractRoute, Modifier
 using ToolipsSession
 
 """
 ### Uploader <: Toolips.ServerExtension
-- type::Vector{Symbol} - the type of this extension.
-- directory::String - The directory to put temporary uploads.
+- type::Vector{Symbol} - the type of this extension. In the case of `Uploader`,
+this will always be [:routing, :connection].
+- directory::String - The directory to put temporary uploads. By default this is
+`uploads/`. Uploader will remove these files after the `FileModifier` function
+provided is ran. This is provided by the `upload_f argument.`
 - lastupload::Dict{String, String} - A dictionary containing ips and files for the last
 upload of each `Connection`.
 - f::Function - The routing extension function that creates the upload route.\n
 The Uploader ServerExtension handles incoming client uploads on the server side.
+As an end user, do not worry about this.
 ##### example
 ```
+tree .
+- src
+   = Example.jl
+= Project.toml
+= Manifest.toml
+- public
+- uploads
+```
+```
+using Toolips
+using ToolipsUploader
 
 ```
 ------------------
 ##### constructors
-- Uploader(directory::String = "uploads", upload_f::Function = uploadsave)
+- Uploader(upload_f::Function = uploadsave, directory::String = "uploads")
 """
 mutable struct Uploader <: ServerExtension
     type::Vector{Symbol}
     directory::String
     lastupload::Dict{String, Any}
     f::Function
-    function Uploader(directory::String = "uploads",
-        upload_f::Function = uploadsave)
+    function Uploader(upload_f::Function = uploadsave,
+        directory::String = "uploads")
         lastupload = Dict{String, String}()
         f(rs::Vector{AbstractRoute}, es::Vector{ServerExtension}) = begin
             rs["/uploader/upload"] = upload_f
@@ -100,19 +115,31 @@ mutable struct FileComponenttModifier <: AbstractComponentModifier
     end
 end
 
-progress(f::Function, fm::FileModifier) = begin
+#==
+TODO
+The FileModifier interface. Each of these will be primarily ClientModifier-based
+functions all with the argument "client", Bool, default true, key-word. This
+will allow us to determine whether or not to send it to Julia. Eventually I would
+also like to have an abort! function to trigger the event abort() in the document.
+==#
+verify(f::Function, finput::Component{:input}) = begin
 
 end
 
-error(f::Function, fm::FileModifier) = begin
+progress!(finput::Component{:input}, prog::Component{:progress}) = begin
 
 end
 
-abort(f::Function, fm::FileModifier) = begin
+error!(f::Function, fm::FileModifier) = begin
+
+end
+
+abort!(f::Function, fm::FileModifier) = begin
 
 end
 
 read(f::Function, fm::FileModifier, a::String) = read(f, fm.file, a)
+
 read(fm::FileModifier, T::Type) = read(fm.file, T)
 
 """
@@ -147,25 +174,42 @@ function fileinput(f::Function, c::Connection, name::String = "",
     inp::Component{:input} = input(name * "input", type = "file",
      name = "fname", p ..., args ...)
     inp["oninput"] = """readFile$name(this);"""
+    # we will need to concat at //CUTOFF in the future.
     sendscript::Component{:script} = script("readscript$name", text = """
     function readFile$name(input) {
-  let file = input.files[0];
+        var file = _("$name").files[0];
+        try{
+            // future do function to restrict filetype
+            // file.name file.size file.type
+        }
+    catch
+        {
 
-  let reader = new FileReader();
+        }
+        var formdata = new FormData();
+        formdata.append("file1", file);
+        var ajax = new XMLHttpRequest();
+        try{
+        // ajax.upload.addEventListener("progress", $(name)progressHandler, false);
+        }
+        try{
+        ajax.addEventListener("load", $(name)completeHandler, false);
+        }
+    catch{
 
-  reader.readAsText(file);
-  var body = document.getElementsByTagName('body')[0].innerHTML;
-  reader.onload = function() {
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", "/uploader/upload");
-      xhr.setRequestHeader("Accept", "application/json");
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.onload = eval(xhr.responseText);
-      xhr.send(reader.result + "?UP?:" + file.name);
-  };
-
-  reader.onerror = function() {
-    console.log(reader.error);
+        }
+        try{
+        ajax.addEventListener("error", $(name)errorHandler, false);
+        }
+    catch{
+        }
+        try{
+        ajax.addEventListener("error", $(name)errorHandler, false);
+        }
+    catch{
+        }
+        ajax.open("POST", "/uploader/upload");
+        ajax.send(formdata);
   };
 
 }""")
@@ -177,7 +221,11 @@ function fileinput(f::Function, c::Connection, name::String = "",
         f(FileModifier(cm, fname))
         rm(c[:Uploader].lastupload[ip])
     end
-    inp
+    inp::Component{:input}
+end
+
+function area_fileinput()
+
 end
 
 export Uploader, fileinput
