5b8b8c1770ebf2206437e461654ce4b5004c8c0f
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-17 13:32:45 -0600

    huge `0.3` `SessionCommand`-based rework

--- src/ToolipsUploader.jl
+++ src/ToolipsUploader.jl
@@ -12,144 +12,64 @@ The toolips uploader provides both a server extension for handling incoming serv
 - [**Toolips**](https://github.com/ChifiSource/Toolips.jl)
 """
 module ToolipsUploader
-using ToolipsDefaults
 import Base: read
 using Toolips
-import Toolips: ServerExtension, AbstractRoute, Modifier
+using Toolips.Components
+import Toolips: AbstractRoute, Modifier
 using ToolipsSession
-import ToolipsSession: AbstractComponentModifier, InputMap
+import ToolipsSession: AbstractComponentModifier, InputMap, bind, do_session_command, register!
+
+function empty_binding(cm::ComponentModifier) end
 
 mutable struct UploadMap <: InputMap
-
+    init::Function
+    progress::Function
+    complete::Function
+    UploadMap() = new(empty_binding, empty_binding, empty_binding)::UploadMap
 end
 
-function bind!(f::Function, um::UploadMap, inp::Component{:input})
-    inp["oninput"] = """readFile$name(this);"""
-    # we will need to concat at //CUTOFF in the future.
-    sendscript::Component{:script} = script("readscript$name", text = """
-    function readFile$name(input) {
-        var file = _("$name").files[0];
-        try{
-            // future do function to restrict filetype
-            // file.name file.size file.type
-        }
-    catch
-        {
-
-        }
-        var formdata = new FormData();
-        formdata.append("file1", file);
-        var ajax = new XMLHttpRequest();
-        try{
-        // ajax.upload.addEventListener("progress", $(name)progressHandler, false);
-        }
-        try{
-        ajax.addEventListener("load", $(name)completeHandler, false);
-        }
-    catch{
-
-        }
-        try{
-        ajax.addEventListener("error", $(name)errorHandler, false);
-        }
-    catch{
-        }
-        try{
-        ajax.addEventListener("error", $(name)errorHandler, false);
-        }
-    catch{
-        }
-        ajax.open("POST", "/uploader/upload");
-        ajax.send(formdata);
-  };
-
-}""")
-    push!(inp.extras, sendscript)
+function do_session_command(c::AbstractConnection, command::Type{ToolipsSession.SessionCommand{:UPL}}, raw::String)
+    @warn raw
+    write!(c, "alert('hi');")
 end
 
-"""
-"""
-mutable struct FileComponentModifier <: AbstractComponentModifier
-    rootc::Vector{Servable}
-    f::Function
-    changes::Vector{String}
-    message::String
-    progress::Float64
-    bytes::Pair{Int64, Int64}
-    file::Toolips.File
-    function FileModifier(cm::ComponentModifier, dir::String)
-        rootc = cm.rootc
-        changes = cm.changes
-        f = cm.f
-        new(rootc, f, changes, File(dir))
+function bind(c::AbstractConnection, fileinput::Component{:fileinput}, um::UploadMap)
+    init_ref = Toolips.gen_ref(8)
+    complete_ref = Toolips.gen_ref(8)
+    upload_id::String = Toolips.gen_ref(10)
+    register!(um.init, c, init_ref)
+    register!(um.complete, c, complete_ref)
+    scr = script(text = """setTimeout(function () { document.getElementById("$(fileinput.name)").onchange = async () => {
+            let fileInput = document.getElementById("$(fileinput.name)");
+			let file = fileInput.files[0];
+			let totalBytes = file.size;
+			let chunkSize = 1024 * 64; // 64 KB
+			let offset = 0;
+
+            sendpage("$init_ref")
+			while (offset < totalBytes) {
+				let slice = file.slice(offset, offset + chunkSize);
+				let chunk = await slice.arrayBuffer();       // raw bytes
+				let bytes = new Uint8Array(chunk);           // Uint8Array view
+
+				offset += bytes.length;
+				// here you literally have the raw `bytes`
+				// e.g., bytes[0], bytes[1], etc.
+                sendinfo('UPL|!|' + offset + ';!;' + totalBytes + ';!;' + bytes);
+			}
+            sendpage("$complete_ref")
+        };}, 500);""")
+    push!(fileinput[:extras], scr)
+    if ~(haskey(c.data, :uploads))
+
+    else
+        push!(c.data[:uploads], )
     end
-    function FileModifier(html::String, dir::String)
-        rootc = ToolipsSession.htmlcomponent(html)
-        f(c::Connection) = begin
-            write!(c, join(changes))
-        end
-        changes = Vector{String}()
-        new(rootc, f, changes, File(dir))
-    end
-end
-#==
-TODO
-The FileModifier interface. Each of these will be primarily ClientModifier-based
-functions all with the argument "client", Bool, default true, key-word. This
-will allow us to determine whether or not to send it to Julia. Eventually I would
-also like to have an abort! function to trigger the event abort() in the document.
-==#
-verify(f::Function, finput::Component{:input}) = begin
-
+    nothing::Nothing
 end
 
-progress!(finput::Component{:input}, prog::Component{:progress}) = begin
-
-end
-
-error!(f::Function) = begin
-
-end
-
-abort!(f::Function, fm::FileModifier) = begin
-
-end
-
-read(f::Function, fm::FileModifier, a::String) = read(f, fm.file, a)
-
-read(fm::FileModifier, T::Type) = read(fm.file, T)
-
-"""
-**Uploader Interface**
-### fileinput(name::String = "", f::Function, c::Connection)
-------------------
-Creates a new fileinput Component, which will upload files to an `Uploader`.
-The `f` function should take a ComponentModifier and a String as positional
-arguments. Files will be removed automatically so long as an exception is not
-thrown.
-#### example
-```
-myuploader = ToolipsUploader.fileinput(c,
-"pizza") do cm::ComponentModifier, file::String
-    try
-        readstr = read(file, String)
-        style!(cm, uploaderbox, "height" => "250px")
-        set_children!(cm, "uploaderbox", components(tmd("customtmd", readstr)))
-    catch
-        rm(file)
-        errora = a("errora",
-        text = "Error! You probably uploaded the wrong type of file, didn't you?")
-        style!(errora, "color" => "red")
-        style!(cm, uploaderbox, "height" => "20px")
-        set_children!(cm, "uploaderbox", components(errora))
-    end
-end
-```
-"""
-function fileinput(name::String = "",
-    p::Pair{String, String} ... ; args ...)
-    input(name * "input", type = "file",
-     name = "fname", p ..., args ...)::Component{:input}
+function fileinput(name::String = "", p::Pair{String, String} ... ; args ...)
+    Component{:fileinput}(name, p ..., type = "file", files = "-", tag = "input"; args ...)
 end
 
 function area_fileinput()
